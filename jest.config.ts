ca10b7b955704efdd5f435271d81c27fbbd19616
Marcos Melo <marcosmelo012@gmail.com>
2025-05-16 12:33:55 -0300

    chore: add jest

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+*/
+
+import type { Config } from 'jest'
+
+const config: Config = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  coverageProvider: 'v8',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+
+}
+
+export default config
