45c99ed7de0976ffdefa0efdf4481dbebcd9f18a
Marcos Melo <marcosmelo012@gmail.com>
2025-05-17 10:49:30 -0300

    test: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -3,6 +3,7 @@ import { MissingParamError } from '../../errors/missing-params-errors'
 import { SignUpController } from './signup'
 import { describe, it, expect } from '@jest/globals'
 import { EmailValidator } from '../controllers/protocols/email-validator'
+import { ServerError } from '../../errors/server-error'
 
 interface SutTypes {
   sut: SignUpController,
@@ -106,5 +107,25 @@ describe('SignUp Controller', () => {
     }
     sut.handle(httpRequest)
     expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+  test('Should return 500 if EmailValidator throws', () => {
+    class EmailValidatorStub implements EmailValidator {
+      isValid (email: string): boolean {
+        throw new Error()
+      }
+    }
+    const emailValidatorStub = new EmailValidatorStub()
+    const sut = new SignUpController(emailValidatorStub)
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'invalid_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
   })
 })
